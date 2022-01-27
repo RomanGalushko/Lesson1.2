48adddc20de877c6877b64bd4c7128be3e9357a1
Roman <ramzos@rambler.ru>
2022-01-28 00:49:27 +0300

    Task1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        byte first=5;
+        short second=500;
+        int three=50_000;
+        long four=500_000L;
+        float five=5.5F;
+        double six=5.55;
+        char seven=100;
+        boolean eight=8>first;
+        boolean nine=8>second;
+        System.out.println("Правда или Ложь :"+" 8>5 -"+eight+"  8>500 - "+nine);
+
     }
 }
